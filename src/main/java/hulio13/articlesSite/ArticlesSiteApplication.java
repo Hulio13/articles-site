903b97d927f1fbb6d778d3d20b5917f91ee52426
hulio13 <andrej.tsyn@yandex.ru>
2023-04-23 20:28:47 +0300

    Initial commit

---
+++ src/main/java/hulio13/articlesSite/ArticlesSiteApplication.java
@@ -0,0 +1,13 @@
+package hulio13.articlesSite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArticlesSiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArticlesSiteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/hulio13/articlesSite/ArticlesSiteApplicationTests.java
@@ -0,0 +1,13 @@
+package hulio13.articlesSite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArticlesSiteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
