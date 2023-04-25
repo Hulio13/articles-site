52b2ff5f0378a65898b095d90cf083525d9e1626
hulio13 <andrej.tsyn@yandex.ru>
2023-04-26 00:49:52 +0300

    Create AuthorName

---
+++ src/main/java/hulio13/articlesApi/domain/entity/author/AuthorName.java
@@ -0,0 +1,26 @@
+package hulio13.articlesApi.domain.entity.author;
+
+import hulio13.articlesApi.exception.IllegalStringLengthException;
+import jakarta.persistence.AttributeOverride;
+import jakarta.persistence.Column;
+import jakarta.persistence.Embeddable;
+import lombok.AccessLevel;
+import lombok.NoArgsConstructor;
+
+@Embeddable
+@NoArgsConstructor(force = true, access = AccessLevel.PROTECTED)
+@AttributeOverride(name = "Value", column = @Column(name = "name"))
+public class AuthorName {
+    public static final int MAX_LENGTH = 50;
+
+    public final String Value;
+
+    public AuthorName(String value){
+        if (value.length() > MAX_LENGTH){
+            throw new IllegalStringLengthException(
+                    "Name length should be less or equal than " + MAX_LENGTH);
+        }
+
+        Value = value;
+    }
+}
