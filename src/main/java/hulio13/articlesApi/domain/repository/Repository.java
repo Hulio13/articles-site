65b78c9ffe81d8fe762d98e641d2ccf2d851a7b5
hulio13 <andrej.tsyn@yandex.ru>
2023-05-03 00:27:16 +0300

    Add Repository

---
+++ src/main/java/hulio13/articlesApi/domain/repository/Repository.java
@@ -0,0 +1,18 @@
+package hulio13.articlesApi.domain.repository;
+
+import java.util.List;
+import java.util.function.Predicate;
+
+public interface Repository<T> {
+    T getById(long id);
+
+    List<T> getAll();
+
+    List<T> getAllIf(Predicate<T> predicate);
+
+    void add(T entity);
+
+    void remove(T entity);
+
+    void removeById(long id);
+}
