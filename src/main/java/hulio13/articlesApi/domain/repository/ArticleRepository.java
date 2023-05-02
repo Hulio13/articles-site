0e2a3379a0da61cdde1e326845c6a15ae602063c
hulio13 <andrej.tsyn@yandex.ru>
2023-05-03 00:27:27 +0300

    Add repositories

---
+++ src/main/java/hulio13/articlesApi/domain/repository/ArticleRepository.java
@@ -0,0 +1,6 @@
+package hulio13.articlesApi.domain.repository;
+
+import hulio13.articlesApi.domain.entity.Article;
+
+public interface ArticleRepository extends Repository<Article> {
+}
---
+++ src/main/java/hulio13/articlesApi/domain/repository/AuthorRepository.java
@@ -0,0 +1,7 @@
+package hulio13.articlesApi.domain.repository;
+
+import hulio13.articlesApi.domain.entity.Author;
+
+public interface AuthorRepository extends Repository<Author> {
+
+}
