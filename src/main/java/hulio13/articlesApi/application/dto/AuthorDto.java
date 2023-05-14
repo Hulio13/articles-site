d993e7f30552d91960d60f61e90a61deed12f50e
hulio13 <andrej.tsyn@yandex.ru>
2023-05-14 21:33:55 +0300

    Add toDomainObject method

--- src/main/java/hulio13/articlesApi/application/dto/AuthorDto.java
+++ src/main/java/hulio13/articlesApi/application/dto/AuthorDto.java
@@ -1,16 +1,23 @@
 package hulio13.articlesApi.application.dto;
 
 import hulio13.articlesApi.domain.entity.Author;
+import hulio13.articlesApi.domain.entity.author.AuthorName;
+import lombok.Data;
 
+@Data
 public class AuthorDto {
-    public long id;
-    public String name;
+    private long id;
+    private String name;
 
-    static public AuthorDto toDto(Author author){
+    public static AuthorDto toDto(Author author){
         AuthorDto dto = new AuthorDto();
         dto.id = author.getId();
         dto.name = author.getName().Value;
 
         return dto;
+    }
+
+    public static Author toDomainObject(AuthorDto dto){
+        return new Author(dto.id, new AuthorName(dto.name));
     }
 }
