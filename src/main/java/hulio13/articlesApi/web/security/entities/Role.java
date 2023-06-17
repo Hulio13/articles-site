805411acc5766c25a117efe6a9f55dd940ea1bca
hulio13 <andrej.tsyn@yandex.ru>
2023-06-18 02:09:35 +0300

    Add Role class for AppUserDetails.

---
+++ src/main/java/hulio13/articlesApi/web/security/entities/Role.java
@@ -0,0 +1,31 @@
+package hulio13.articlesApi.web.security.entities;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+public class Role {
+    @Id
+    private String role;
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+
+        Role role1 = (Role) o;
+
+        return role.equals(role1.role);
+    }
+
+    @Override
+    public int hashCode() {
+        return role.hashCode();
+    }
+}
