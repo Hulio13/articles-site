1cb129d200eb89825bf593c0c2049201272dc061
hulio13 <andrej.tsyn@yandex.ru>
2023-06-18 02:11:54 +0300

    Add UserController

---
+++ src/main/java/hulio13/articlesApi/web/controllers/UserController.java
@@ -0,0 +1,42 @@
+package hulio13.articlesApi.web.controllers;
+
+import hulio13.articlesApi.domain.entity.author.AuthorName;
+import hulio13.articlesApi.web.security.entities.AppUserDetails;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.UserDetailsManager;
+import org.springframework.web.bind.annotation.*;
+
+
+@RestController
+@RequestMapping("/api")
+@PreAuthorize("permitAll()")
+public class UserController {
+    private final UserDetailsManager userDetailsManager;
+    private final PasswordEncoder encoder;
+
+    public UserController(UserDetailsManager userDetailsManager, PasswordEncoder encoder) {
+        this.userDetailsManager = userDetailsManager;
+        this.encoder = encoder;
+    }
+
+    @PostMapping("/register/{username}/{password}/{name}")
+    public ResponseEntity<Boolean> register(@PathVariable String username, @PathVariable String password, @PathVariable String name) {
+        AppUserDetails appUserDetails = new AppUserDetails();
+        appUserDetails.setUsername(username);
+        appUserDetails.setPassword(encoder.encode(password));
+        appUserDetails.getAuthor().setName(new AuthorName(name));
+        appUserDetails.addRoleIfNotExist("ROLE_AUTHOR");
+
+        userDetailsManager.createUser(appUserDetails);
+
+        return ResponseEntity.ok(true);
+    }
+
+    @PutMapping("/changePass/{oldPass}/{newPass}")
+    public ResponseEntity<Boolean> changePass(@PathVariable String oldPass, @PathVariable String newPass) {
+        userDetailsManager.changePassword(oldPass, newPass);
+        return ResponseEntity.ok(true);
+    }
+}
