908ed58a63626eef5fcbf4ee45cd9078d5a42745
hulio13 <andrej.tsyn@yandex.ru>
2023-06-19 21:23:15 +0300

    Create AlreadyExistException

---
+++ src/main/java/hulio13/articlesApi/infrastructure/data/AlreadyExistException.java
@@ -0,0 +1,14 @@
+package hulio13.articlesApi.infrastructure.data;
+
+public class AlreadyExistException extends RuntimeException {
+    public AlreadyExistException() {
+    }
+
+    public AlreadyExistException(String message) {
+        super(message);
+    }
+
+    public AlreadyExistException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
