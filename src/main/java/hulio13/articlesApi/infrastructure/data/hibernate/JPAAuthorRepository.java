aed36873225ec2e8bbb2dac635cf6c59e8df6792
hulio13 <andrej.tsyn@yandex.ru>
2023-06-19 21:22:48 +0300

    Add create and update methods, add more validation logic

--- src/main/java/hulio13/articlesApi/infrastructure/data/hibernate/JPAAuthorRepository.java
+++ src/main/java/hulio13/articlesApi/infrastructure/data/hibernate/JPAAuthorRepository.java
@@ -1,9 +1,12 @@
 package hulio13.articlesApi.infrastructure.data.hibernate;
 
+import hulio13.articlesApi.api.exceptions.NotFoundException;
 import hulio13.articlesApi.domain.entity.Author;
 import hulio13.articlesApi.domain.repository.AuthorRepository;
+import hulio13.articlesApi.infrastructure.data.AlreadyExistException;
 import jakarta.persistence.EntityManager;
 import jakarta.persistence.PersistenceContext;
+import org.jetbrains.annotations.NotNull;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
@@ -27,17 +30,50 @@ public class JPAAuthorRepository implements AuthorRepository {
 
     @Override
     public void remove(Author entity) {
-        em.remove(entity);
+        var author = getById(entity.getId());
+        if (author.isEmpty())
+            throw new NotFoundException("Author with id '" + entity.getId() +
+                    "' not found");
+
+        em.remove(author.get());
     }
 
     @Override
     public void removeById(long id) {
-        getById(id).ifPresent(a -> em.remove(a));
+        var author = getById(id).orElseThrow(() -> new NotFoundException("Author with id '" + id +
+                "' not found"));
+
+        em.remove(author);
     }
 
     @Override
-    public Author save(Author entity) {
+    public Author create(Author entity) {
+        if (entity == null) throw new IllegalArgumentException("Entity is null, Author expected.");
+
+        if (getById(entity.getId()).isPresent())
+            throw new AlreadyExistException("Author with id '" + entity.getId()+ "' already exist.");
+
+        Optional<Author> byAuthorName = getByAuthorName(entity.getName().Value);
+        if (byAuthorName.isPresent())
+            throw new AlreadyExistException("Author with name '" + entity.getName()+ "' already exist.");
+
         em.persist(entity);
         return entity;
+    }
+
+    @Override
+    public Author update(Author entity) {
+        var author = getById(entity.getId());
+        if (author.isEmpty())
+            throw new NotFoundException("Author with id '" + entity.getId() +
+                    "' not found");
+
+        return em.merge(entity);
+    }
+
+    @Override
+    public Optional<Author> getByAuthorName(@NotNull String name) {
+        return em.createQuery("select a from Author a where a.name.Value = '" + name + "'", Author.class)
+                        .getResultStream().findFirst();
     }
 }
