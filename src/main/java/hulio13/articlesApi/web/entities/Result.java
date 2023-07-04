db575457b0fe5ad3a20960583df1e1688338b58e
hulio13 <andrej.tsyn@yandex.ru>
2023-07-05 00:33:21 +0300

    Add ok and error static methods

--- src/main/java/hulio13/articlesApi/web/entities/Result.java
+++ src/main/java/hulio13/articlesApi/web/entities/Result.java
@@ -1,4 +1,11 @@
 package hulio13.articlesApi.web.entities;
 
 public record Result<T>(boolean isSuccess, T data, Error error) {
+    public static <T> Result<T> ok(T entity) {
+        return new Result<>(true, entity, null);
+    }
+
+    public static <T> Result<T> error(int errCode, String msg){
+        return new Result<>(false, null, new Error(errCode, msg));
+    }
 }
